fd415f3218f8a927a341daaae84e821c32f095c7
Josep Conejero <bermeu@gmail.com>
2022-06-04 18:18:17 +0200

    Added notes paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import Key from "./components/Key/Key";
 
-const notes = [""];
+const notes = [
+  "notes/01-1-Do.mp3",
+  "notes/02-1-DoSharp.mp3",
+  "notes/03-1-Re.mp3",
+  "notes/04-1-ReSharp.mp3",
+  "notes/05-1-Mi.mp3",
+  "notes/06-1-Fa.mp3",
+  "notes/07-1-FaSharp.mp3",
+  "notes/08-1-Sol.mp3",
+  "notes/09-1-SolSharp.mp3",
+  "notes/10-1-La.mp3",
+  "notes/11-1-LaSharp.mp3",
+  "notes/12-1-Si.mp3",
+  "notes/13-2-Do.mp3",
+  "notes/14-2-DoSharp.mp3",
+  "notes/15-2-Re.mp3",
+  "notes/16-2-ReSharp.mp3",
+  "notes/17-2-Mi.mp3",
+  "notes/18-2-Fa.mp3",
+  "notes/19-2-FaSharp.mp3",
+  "notes/20-2-Sol.mp3",
+  "notes/21-2-SolSharp.mp3",
+  "notes/22-2-La.mp3",
+  "notes/23-2-LaSharp.mp3",
+  "notes/24-2-Si.mp3",
+  "notes/25-3-Si.mp3",
+  "notes/26-3-Do.mp3",
+  "notes/27-3-DoSharp.mp3",
+  "notes/28-3-Re.mp3",
+  "notes/29-3-ReSharp.mp3",
+  "notes/30-3-Mi.mp3",
+  "notes/31-3-Fa.mp3",
+  "notes/32-3-FaSharp.mp3",
+  "notes/33-3-Sol.mp3",
+  "notes/34-3-SolSharp.mp3",
+  "notes/35-3-La.mp3",
+  "notes/36-3-LaSharp.mp3",
+  "notes/37-3-Si.mp3",
+];
 
 function App() {
   return (
