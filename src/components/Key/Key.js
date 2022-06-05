ca70e8334293d9f280914774022c9e0fc61e4d8d
Josep Conejero <bermeu@gmail.com>
2022-06-05 13:10:48 +0200

    Added css to keys

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -6,24 +6,44 @@ const WhiteKey = styled.div`
   height: 200px;
   background-color: white;
   border: solid black 2px;
+  text-align: center;
+`;
+
+const BlackKey = styled.div`
+  width: 30px;
+  height: 125px;
+  background-color: black;
+  border: solid white 2px;
+  color: white;
+  text-align: center;
 `;
 
 export const Key = ({ keySound, note }) => {
-  /*  let keySound = new Howl({
-    src: [sound],
-  }); */
-
   const playNote = () => {
     keySound.play();
   };
 
-  /*   const stopNote = () => {
-    sound.stop();
-  }; */
-
   return (
     <>
-      <WhiteKey onClick={playNote}>{note}</WhiteKey>
+      {note === 2 ||
+      note === 4 ||
+      note === 7 ||
+      note === 9 ||
+      note === 11 ||
+      note === 14 ||
+      note === 16 ||
+      note === 19 ||
+      note === 21 ||
+      note === 23 ||
+      note === 26 ||
+      note === 28 ||
+      note === 31 ||
+      note === 33 ||
+      note === 35 ? (
+        <BlackKey onClick={playNote}>{note}</BlackKey>
+      ) : (
+        <WhiteKey onClick={playNote}>{note}</WhiteKey>
+      )}
     </>
   );
 };
